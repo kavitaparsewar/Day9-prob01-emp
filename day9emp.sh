d245eb9429f4bce221c48e013a029ae38fbfa2bc
kavitaparsewar <parsewarkavita@gmail.com>
2021-08-19 22:02:01 +0530

    employee present or absent is added succesfully

--- day9emp.sh
+++ day9emp.sh
@@ -1,2 +1,8 @@
 #!/bin/bash -x
-echo "welcome to employee wage computation program on Master Branch"
+empcheck=$((RANDOM%2))
+if  [ $empcheck -eq 1 ]
+then
+        echo Employee is present
+else
+        echo Employee is absent
+fi
