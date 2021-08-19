defe0cfb99178b19ba9e5f98cb0a2e87b7eb9070
kavitaparsewar <parsewarkavita@gmail.com>
2021-08-19 22:36:14 +0530

    added UC5

--- day9emp.sh
+++ day9emp.sh
@@ -1,19 +1,27 @@
 #!/bin/bash -x
+
 empRatePerHr=20
 isFullTime=1
 isPartTime=2
-empcheck=$((RANDOM%3))
+totalsalary=0
+numofworkingDays=10
 
-case "$empcheck" in
-     $isFullTime)
-        empHrs=8
+for (( day=1; day<=$numofworkingDays; day++ ))
+do
+   empcheck=$((RANDOM%3))
+        case "$empcheck" in
+          $isFullTime)
+      empHrs=8
      ;;
      $isPartTime)
-        empHrs=4
+       empHrs=4
      ;;
      *)
-        empHrs=0
+       empHrs=0
      ;;
- esac
+   esac
 wage=$(($empHrs*$empRatePerHr))
+totalEmpHrs=$(($totalEmpHrs+$empHrs))
+done
+
 
